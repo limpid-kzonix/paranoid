0f982981eb9a59e13f93df9323aa9d6afd61a7ce
limpid-kzonix <balyszyn@gmail.com>
2024-08-26 21:06:21 +0300

    feat(CS32F103C8T6): Initial tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,124 +2,23 @@
 
 // put function declarations here:
 int myFunction(int, int);
-#define LED_PIN_1 A6
-#define LED_PIN_2 A7
+#define LED_PIN PC13
 
 
 void setup()
 {
   // Initialize the digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN_1, OUTPUT);
-  pinMode(LED_PIN_2, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 
 void loop()
 {
 
-  delay(1000);
   // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(3000); // Wait for a second
+  digitalWrite(LED_PIN, HIGH);
+  delay(50); // Wait for a second
 
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(1000); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_2, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_2, LOW);
-  delay(100); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a secon
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a second
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(100); // Wait for a second
-    // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(100); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(10); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(10); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN_1, HIGH);
-  delay(10); // Wait for a second
-
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN_1, LOW);
-  delay(10); // Wait for a second
-}
-
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  digitalWrite(LED_PIN, LOW);
+  delay(50); // Wait for a second
 }
